25be47a033724aba8c17768e25bf13a5889f37f6
Travis Keep <keep94@gmail.com>
2021-03-12 09:24:07 -0800

    Use jszwec/csvutil

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/keep94/mvn_dependency_tree
 
 go 1.14
+
+require github.com/jszwec/csvutil v1.5.0
--- model.go
+++ model.go
@@ -6,129 +6,34 @@ import (
 	"io"
 	"os"
 	"sort"
+
+	"github.com/jszwec/csvutil"
 )
 
 type Library struct {
-	Name        string
-	NewLocation string
-	Latest      string
-	Description string
-}
-
-type libraryList []Library
-
-func (s libraryList) Len() int { return len(s) }
-
-func (_ libraryList) NumFields() int { return 4 }
-
-func (_ libraryList) Title(fields []string) {
-	fields[0] = "name"
-	fields[1] = "new_location"
-	fields[2] = "latest"
-	fields[3] = "description"
-}
-
-func (s libraryList) Get(index int, fields []string) {
-	fields[0] = s[index].Name
-	fields[1] = s[index].NewLocation
-	fields[2] = s[index].Latest
-	fields[3] = s[index].Description
-}
-
-func (s *libraryList) Add(fields []string) {
-	*s = append(*s, Library{
-		Name:        fields[0],
-		NewLocation: fields[1],
-		Latest:      fields[2],
-		Description: fields[3],
-	})
+	Name        string `csv:"name"`
+	NewLocation string `csv:"new_location"`
+	Latest      string `csv:"latest"`
+	Description string `csv:"description"`
 }
 
 type VersionKey struct {
-	Name    string
-	Version string
+	Name    string `csv:"name"`
+	Version string `csv:"version"`
 }
 
 type Version struct {
 	VersionKey
-	Date string
-}
-
-type versionList []Version
-
-func (s versionList) Len() int { return len(s) }
-
-func (_ versionList) NumFields() int { return 3 }
-
-func (_ versionList) Title(fields []string) {
-	fields[0] = "name"
-	fields[1] = "version"
-	fields[2] = "date"
-}
-
-func (s versionList) Get(index int, fields []string) {
-	fields[0] = s[index].Name
-	fields[1] = s[index].Version
-	fields[2] = s[index].Date
-}
-
-func (s *versionList) Add(fields []string) {
-	*s = append(*s, Version{
-		VersionKey: VersionKey{
-			Name:    fields[0],
-			Version: fields[1],
-		},
-		Date: fields[2],
-	})
+	Date string `csv:"date"`
 }
 
 type Dependency struct {
 	VersionKey
-	Date        string
-	Latest      string
-	LatestDate  string
-	NewLocation string
-	Description string
-}
-
-type dependencyList []Dependency
-
-func (s dependencyList) Len() int { return len(s) }
-
-func (_ dependencyList) NumFields() int { return 7 }
-
-func (_ dependencyList) Title(fields []string) {
-	fields[0] = "name"
-	fields[1] = "version"
-	fields[2] = "date"
-	fields[3] = "latest"
-	fields[4] = "latest_date"
-	fields[5] = "new_location"
-	fields[6] = "description"
-}
-
-func (s dependencyList) Get(index int, fields []string) {
-	fields[0] = s[index].Name
-	fields[1] = s[index].Version
-	fields[2] = s[index].Date
-	fields[3] = s[index].Latest
-	fields[4] = s[index].LatestDate
-	fields[5] = s[index].NewLocation
-	fields[6] = s[index].Description
-}
-
-func (s *dependencyList) Add(fields []string) {
-	*s = append(*s, Dependency{
-		VersionKey: VersionKey{
-			Name:    fields[0],
-			Version: fields[1],
-		},
-		Date:        fields[2],
-		Latest:      fields[3],
-		LatestDate:  fields[4],
-		NewLocation: fields[5],
-		Description: fields[6],
-	})
+	Date        string `csv:"date"`
+	Latest      string `csv:"latest"`
+	LatestDate  string `csv:"latest_date"`
+	NewLocation string `csv:"new_location"`
+	Description string `csv:"description"`
 }
 
 type LibraryDB map[string]Library
@@ -241,7 +146,7 @@ func (db VersionDB) Versions() []Version {
 
 func ReadLibrariesFile(path string) ([]Library, error) {
 	var result []Library
-	if err := readCsvFile(path, (*libraryList)(&result)); err != nil {
+	if err := readCsvFile(path, &result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -249,7 +154,7 @@ func ReadLibrariesFile(path string) ([]Library, error) {
 
 func ReadVersionsFile(path string) ([]Version, error) {
 	var result []Version
-	if err := readCsvFile(path, (*versionList)(&result)); err != nil {
+	if err := readCsvFile(path, &result); err != nil {
 		return nil, err
 	}
 	return result, nil
@@ -257,26 +162,26 @@ func ReadVersionsFile(path string) ([]Version, error) {
 
 func ReadDependenciesFile(path string) ([]Dependency, error) {
 	var result []Dependency
-	if err := readCsvFile(path, (*dependencyList)(&result)); err != nil {
+	if err := readCsvFile(path, &result); err != nil {
 		return nil, err
 	}
 	return result, nil
 }
 
 func WriteLibrariesFile(path string, libraries []Library) error {
-	return writeCsvFile(path, libraryList(libraries))
+	return writeCsvFile(path, libraries)
 }
 
 func WriteVersionsFile(path string, versions []Version) error {
-	return writeCsvFile(path, versionList(versions))
+	return writeCsvFile(path, versions)
 }
 
 func WriteDependenciesFile(path string, dependencies []Dependency) error {
-	return writeCsvFile(path, dependencyList(dependencies))
+	return writeCsvFile(path, dependencies)
 }
 
 func WriteDependencies(w io.Writer, dependencies []Dependency) error {
-	return writeCsv(w, dependencyList(dependencies))
+	return writeCsv(w, dependencies)
 }
 
 type Merger interface {
@@ -292,62 +197,36 @@ func MergeAll(dependencies []Dependency, m Merger) error {
 	return nil
 }
 
-func readCsvFile(path string, consumer csvConsumer) error {
+func readCsvFile(path string, aSlicePtr interface{}) error {
 	f, err := os.Open(path)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-	return readCsv(f, consumer)
+	return readCsv(f, aSlicePtr)
 }
 
-func readCsv(r io.Reader, consumer csvConsumer) error {
-	csvReader := csv.NewReader(r)
-	csvReader.ReuseRecord = true
-	csvReader.FieldsPerRecord = consumer.NumFields()
-	// Read title line
-	_, err := csvReader.Read()
+func readCsv(r io.Reader, aSlicePtr interface{}) error {
+	decoder, err := csvutil.NewDecoder(csv.NewReader(r))
 	if err != nil {
 		return err
 	}
-	record, err := csvReader.Read()
-	for err == nil {
-		consumer.Add(record)
-		record, err = csvReader.Read()
-	}
-	if err == io.EOF {
-		return nil
-	}
-	return err
+	return decoder.Decode(aSlicePtr)
 }
 
-func writeCsvFile(path string, producer csvProducer) error {
+func writeCsvFile(path string, aSlice interface{}) error {
 	f, err := os.OpenFile(
 		path, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-	return writeCsv(f, producer)
+	return writeCsv(f, aSlice)
 }
 
-func writeCsv(w io.Writer, producer csvProducer) error {
-	csvWriter := csv.NewWriter(w)
-	csvLine := make([]string, producer.NumFields())
-	producer.Title(csvLine)
-	csvWriter.Write(csvLine)
-	if err := csvWriter.Error(); err != nil {
-		return err
-	}
-	for i := 0; i < producer.Len(); i++ {
-		producer.Get(i, csvLine)
-		csvWriter.Write(csvLine)
-		if err := csvWriter.Error(); err != nil {
-			return err
-		}
-	}
-	csvWriter.Flush()
-	return csvWriter.Error()
+func writeCsv(w io.Writer, aSlice interface{}) error {
+	encoder := csvutil.NewEncoder(csv.NewWriter(w))
+	return encoder.Encode(aSlice)
 }
 
 func replace(keyName, fieldName, value string, replace *string) error {
@@ -364,16 +243,4 @@ func replace(keyName, fieldName, value string, replace *string) error {
 			keyName, *replace, value, fieldName)
 	}
 	return nil
-}
-
-type csvProducer interface {
-	Len() int
-	NumFields() int
-	Title(fields []string)
-	Get(index int, fields []string)
-}
-
-type csvConsumer interface {
-	NumFields() int
-	Add(fields []string)
 }
